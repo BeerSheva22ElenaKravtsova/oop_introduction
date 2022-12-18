02690ad7f0c80eda20e5d802c2899dca18bd7c34
ElenaKravtsova <elena@192.168.2.202>
2022-12-19 00:13:00 +0200

    Homework 13 + Classwork 15

--- src/telran/memory/MemoryOperationsTest.java
+++ src/telran/memory/MemoryOperationsTest.java
@@ -2,27 +2,33 @@ package telran.memory;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class MemoryOperationsTest {
+	private static final long MGB = 1024 * 1024 * 1024;
 	byte ar[];
 
 	@Test
+	@Disabled
 	void maxMemoryTest() {
 		int maxMemory = MemoryOperations.getMaxAvailableMemory();
-		ar = new byte[maxMemory];
-		ar = null; // массив у которого нет ссылки, выделенная память становится мусором и гарбедж коллекшион ддолжен сработать
-		
-		//код обработки Exception
 		boolean flException = false;
+		ar = new byte[maxMemory * 1024 * 1024];
+		ar = null;
 		try {
-			ar = new byte[maxMemory + 1];
-		} catch(Throwable e) {
+			ar = new byte[(maxMemory+1) * 1024 * 1024];
+
+		} catch (Throwable e) {
 			flException = true;
 		}
-		assertTrue(flException); // если флаг прошел - значит у нас ошибка тк я попадаю в Exception
-	
-	 
+		assertTrue(flException);
 	}
+	
+	@Test
+	void standardMemoryMethods() {
+		Runtime runtime = Runtime.getRuntime();
+		System.out.printf("Maximal memory JVM may require from OS: %d," + " current total JVM memory: %d, current free JVM memory: %d", runtime.maxMemory() / MGB, runtime.totalMemory() , runtime.freeMemory()); //
+	};
 
 }
